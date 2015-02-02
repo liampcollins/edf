0f2a34903bbb9fd09e31059df9750546b4bf63d8
liamcollins <liampcollins@gmail.com>
2015-02-02 00:49:36 +0000

    sliders working and posting to db

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,37 +1,60 @@
-$(document).ready(function(){
-	// console.log("hello")
-	// $('input[type="range"]').rangeslider({
-	// 	rangeClass: 'rangeslider',
- //    fillClass: 'rangeslider__fill',
- //    handleClass: 'rangeslider__handle'
-	// });
 
+var data = new Object();
+var factors;
+
+function request(method, url, data){
+  return $.ajax({
+    method: method,
+    url: url,
+    dataType: "json",
+    data: data
+  });
+};
 
 
 function refreshValue (){
 	var value = $(this).slider( "value" );
-	var id = $(this)[0].id
-	var counter = '#'+ id + '-counter'
-	$(counter ).text(value)
+	var id = $(this)[0].id.split("-")[0];
+	var counter = '#'+ id;
+	$(counter ).text(value);
+};
+
+function submitVote(){
+	factors = $('.counter');
+	$.each(factors, function(index, value){
+		data[value.id] = parseInt(value.innerHTML)
+	});
+  request("POST", "/votes", {vote:data}).success(function(){
+  	initiateSlider();
+  	$('.counter').innerHTML("");
+  });
+};
+
+
+function initiateSlider(){
+	$(".slider").slider({
+	  orientation: "horizontal",
+	  range: "min",
+	  max: 100,
+	  value: 50,
+	  slide: refreshValue,
+	  change: refreshValue
+	});
 }
 
-$(function() {
-		console.log("hello")
-
-    $( "#finance, #skills, #busenv, #culture, #inf, #diginf, #agglom, #market, #lifestyle" ).slider({
-      orientation: "horizontal",
-      range: "min",
-      max: 100,
-      value: 50,
-      slide: refreshValue,
-      change: refreshValue
-    });
-  });
-
-
+$(document).ready(function(){
+	// $(function() {
+	// 	$(".slider").slider({
+	// 	  orientation: "horizontal",
+	// 	  range: "min",
+	// 	  max: 100,
+	// 	  value: 50,
+	// 	  slide: refreshValue,
+	// 	  change: refreshValue
+	// 	});
+	// });
+	initiateSlider();
+	$('.submit-vote').on('click', submitVote);
 });
-
-	// $('input[type="range"]').rangeslider();
-
 
 
